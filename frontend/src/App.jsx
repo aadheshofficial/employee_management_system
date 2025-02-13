5dfebf409409ebfc00a01b7cbc4e267d5354c3e5
aadheshofficial <aadheshoffcial@gmail.com>
2025-02-13 09:40:14 +0530

    added tailwind css to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
 
   return (
-    <div>welcome to Employee management system</div>
+    <div className='text-3xl text-teal-500'>welcome to Employee management system</div>
   )
 }
 
